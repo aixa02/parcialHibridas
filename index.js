fd1cad172db7628f33d4e43344018f0c698407de
aixa saad <aixa.saad@davinci.edu.ar>
2025-11-21 16:31:48 -0300

    se agrego el cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express"
 import MedicamentosRoute from "./routes/medicamentos.route.js"
 import MedicamentosApiRoute from "./api/routes/medicamentos.api.routes.js"
 import ClientesApiRoute from "./api/routes/clientes.api.routes.js"
+import cors from "cors";
+
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use( express.json() )
-
+app.use(cors());
 app.use("/", MedicamentosRoute);
 
 app.use("/api/medicamentos", MedicamentosApiRoute);
