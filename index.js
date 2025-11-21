7fceeb9bb1421172c9e4ba286fa3e4fc418d5dec
aixa saad <aixa.saad@davinci.edu.ar>
2025-11-21 17:48:40 -0300

    agregando static para las imagenes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}));
 app.use( express.json() )
 app.use(cors());
 app.use("/", MedicamentosRoute);
-
+app.use('/imagenes', express.static('imagenes'));
 app.use("/api/medicamentos", MedicamentosApiRoute);
 app.use("/api/clientes", ClientesApiRoute);
 app.use(express.static('public'));
