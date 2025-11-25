0f50e3014538d5003b089357e4a43b070dd7934d
aixa saad <aixa.saad@davinci.edu.ar>
2025-11-25 17:20:44 -0300

    token

--- api/controllers/usuarios.api.controllers.js
+++ api/controllers/usuarios.api.controllers.js
@@ -7,5 +7,7 @@ export function createUser(req, res) {
 }
 
 export function login(req, res) {
-
+    services.login(req.body)
+        .then(usuario => res.status(201).json(usuario))
+        .catch(err => res.status(400).json(err))
 }
--- services/usuarios.services.js
+++ services/usuarios.services.js
@@ -1,10 +1,12 @@
 import { MongoClient, ObjectId } from "mongodb";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 //instancia de Mongo
 const client = new MongoClient("mongodb+srv://admin:admin@hibridas.r8a3y5f.mongodb.net/")
 // base de datos que nos conectamos
 const db = client.db("AH20232CP1")
+const SECRET_KEY = "dwm4av"
 
 export async function createUser(usuario) {
     await client.connect()
@@ -12,11 +14,26 @@ export async function createUser(usuario) {
     const existe = await db.collection("usuarios").findOne({ email: usuario.email })
     if (existe) throw new Error("No se pudo crear el usuario")
 
-    const usuarioNuevo = { ...usuario, confirmPassword: undefined }
+    const usuarioNuevo = { email: usuario.email, password: usuario.password }
+
     usuarioNuevo.password = await bcrypt.hash(usuario.password, 10)
 
 
     await db.collection("usuarios").insertOne(usuarioNuevo)
     return { ...usuario, password: undefined, confirmPassword: undefined }
+
+}
+
+
+export async function login(usuario) {
+    await client.connect()
+
+    const existe = await db.collection("usuarios").findOne({ email: usuario.email })
+    if (!existe) throw new Error("Credenciales invalidas")
+    const token = jwt.sign({ ...existe, password: undefined, confirmPassword: undefined }, SECRET_KEY, { expiresIn: "2h" })
+
+    const esValido = await bcrypt.compare(usuario.password, existe.password)
+    if (!esValido) throw new Error("Credenciales invalidas")
+    return { ...existe, password: undefined, confirmPassword: undefined, token: token }
 
 }
